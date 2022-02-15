b43779bcbe5be68a8a2fa71abc7387d928e99ebe
aditya chaudhary <chaudharyaditya.in@gmail.com>
2022-02-15 16:24:57 +0530

    1 complete

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,12 +2,18 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+//in redux we pass actions similarly here no more complex actions so we 
+// pass new state and gets it as output directly ...
+function countReducer(state,newState){
+  return newState;
+}
 
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
-
+  // const [count, setCount] = React.useState(initialCount)
+  const [count,setCount]=React.useReducer(countReducer,initialCount);
+  
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
